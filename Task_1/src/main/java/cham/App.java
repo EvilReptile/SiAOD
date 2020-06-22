ce5378ea60a88f38da81e1e3af88e7f3b5f8a282
EvilReptile <green0chameleon@yandex.ru>
2020-06-22 23:22:04 +0300

    Задачи по СиАОДу

---
+++ Task_1/src/main/java/cham/App.java
@@ -0,0 +1,45 @@
+package cham;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class App {
+
+    static HashMap<String, Integer> seq = new HashMap<>();
+
+    public static void main( String[] args ) {
+        Scanner scn = new Scanner(System.in);
+        scn.nextLine();
+        String max = "0";
+        for(String num : scn.nextLine().split(" ")){
+            if(max.hashCode() < num.hashCode())
+                max = num;
+
+            if(seq.get(num) != null)
+                seq.put(num, seq.get(num) + 1);
+            else
+                seq.put(num, 1);
+        }
+
+        System.out.println(calculate(Integer.parseInt(max)));
+    }
+
+    public static int calculate(int size){
+        if(size <= 0)
+            return 0;
+
+        else if(size == 1)
+            if(seq.get("1") != null)
+                return seq.get("1");
+            else
+                return 0;
+
+        int num;
+        if(seq.get(""+size) == null)
+            num = 0;
+        else
+            num = seq.get(""+size) * size;
+
+        return Math.max(calculate(size - 1), calculate(size - 2) + num);
+    }
+
+}
---
+++ Task_2/src/main/java/cham/App.java
@@ -0,0 +1,33 @@
+package cham;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main( String[] args ) {
+        Scanner scn  = new Scanner(System.in);
+        String line = scn.nextLine();
+
+        int abIndex = line.indexOf("AB");
+        if(abIndex != -1){
+            String firstLine = line.substring(0, abIndex);
+            String secondLine = line.substring(abIndex + 2);
+            if(firstLine.indexOf("BA") != -1 || secondLine.indexOf("BA") != -1){
+                System.out.println("YES");
+                return;
+            }
+        }
+
+        abIndex = line.indexOf("BA");
+        if(abIndex != -1){
+            String firstLine = line.substring(0, abIndex);
+            String secondLine = line.substring(abIndex + 2);
+            if(firstLine.indexOf("AB") != -1 || secondLine.indexOf("AB") != -1){
+                System.out.println("YES");
+                return;
+            }
+        }
+
+        System.out.println("NO");
+
+    }
+}
